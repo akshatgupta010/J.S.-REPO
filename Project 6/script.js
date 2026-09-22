e08ce001dc37e3bda0f594ef979c7a7366abe43f
akshatgupta010 <guptaak0707@gmail.com>
2026-09-22 15:58:42 +0530

    Add Project 6

---
+++ Project 6/script.js
@@ -0,0 +1,35 @@
+// generating random color 
+
+const randomColor = function() {
+    const hex = '0123456789ABCDEF' ;
+    let color = '#' ;
+
+    for ( let i = 0 ; i < 6 ; i ++ )
+    {
+       color+=  hex[Math.floor(Math.random()*16)] ;
+    }
+
+    return color ;
+}
+
+let intervalId ;
+
+const startChangingColor = function() {
+
+      if (!intervalId) {                               // IMPORTANT 
+        intervalId = setInterval(changeBgColor,1000);
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null; // flushing out the value -> dereferencing the intervalId value to prevent overriding the value when starting and stopping again and again.
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor) ;
+document.querySelector('#stop').addEventListener('click', stopChangingColor) ;
